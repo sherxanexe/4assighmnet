78703229a2d173f3b3171df682cbacdc13bee02a
sherxanexe <96943761+sherxanexe@users.noreply.github.com>
2022-02-21 08:33:56 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
+    
+    audioElement.addEventListener('ended', function() {
+        this.play();
+    }, false);
+    
+    audioElement.addEventListener("canplay",function(){
+        $("#length").text("Duration:" + audioElement.duration + " seconds");
+        $("#source").text("Source:" + audioElement.src);
+        $("#status").text("Status: Ready to play").css("color","green");
+    });
+    
+    audioElement.addEventListener("timeupdate",function(){
+        $("#currentTime").text("Current second:" + audioElement.currentTime);
+    });
+    
+    $('#play').click(function() {
+        audioElement.play();
+        $("#status").text("Status: Playing");
+    });
+    
+    $('#pause').click(function() {
+        audioElement.pause();
+        $("#status").text("Status: Paused");
+    });
+    
+    $('#restart').click(function() {
+        audioElement.currentTime = 0;
+    })});
